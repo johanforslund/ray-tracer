85ffacf992458e3cb0bece4dc58a80724a02ef83
johanforslund <johanforslund95@gmail.com>
2019-09-17 20:54:04 +0200

    Camera can save image to ppm file

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -16,25 +16,26 @@ Camera::~Camera() {
 
 void Camera::createImage() {
     double maxValue = 0;
-    int image[800][800][3];
     for (int i = 0; i < 800; i++) {
         for (int j = 0; j < 800; j++ ) {
-            if (pixels[i][j].color.r > maxValue) maxValue = pixels[i][j].color.g;
+            if (pixels[i][j].color.r > maxValue) maxValue = pixels[i][j].color.r;
             if (pixels[i][j].color.g > maxValue) maxValue = pixels[i][j].color.g;
             if (pixels[i][j].color.b > maxValue) maxValue = pixels[i][j].color.b;
         }
     }
+
+    FILE *fp = fopen("images/image.ppm", "wb"); /* b - binary mode */
+    (void) fprintf(fp, "P6\n%d %d\n255\n", 800, 800);
+
     for (int i = 0; i < 800; i++) {
         for (int j = 0; j < 800; j++ ) {
-            image[i][j][0] = (pixels[i][j].color.r * 255.99)/maxValue;
-            image[i][j][1] = (pixels[i][j].color.g * 255.99)/maxValue;
-            image[i][j][2] = (pixels[i][j].color.b * 255.99)/maxValue;
+            static unsigned char color[3];
+            color[0] = (pixels[i][j].color.r * 255.99)/maxValue;  /* red */
+            color[1] = (pixels[i][j].color.g * 255.99)/maxValue;  /* green */
+            color[2] = (pixels[i][j].color.b * 255.99)/maxValue;  /* blue */
+            (void) fwrite(color, 1, 3, fp);
         }
     }
 
-    std::cout << image[0][0][0] << std::endl;
-    std::cout << image[0][0][1] << std::endl;
-    std::cout << image[0][0][2] << std::endl;
-
-    // TODO: Write to file
+    (void) fclose(fp);
 }
